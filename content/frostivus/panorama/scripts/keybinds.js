c29e209144e73e464776b2884476ca4eac9c6ab4
CarpeSwag <james@garbagnati.com>
2017-09-21 01:02:10 -0700

    Threw in some custom mouse zoom cause y'know why not?

--- content/frostivus/panorama/scripts/keybinds.js
+++ content/frostivus/panorama/scripts/keybinds.js
@@ -35,3 +35,91 @@ function KeyUpEvent(dir) {
 	Game.AddCommand( "+DKey", OnRightKeyPress, "", 0 );
 	Game.AddCommand( "-DKey", OnRightKeyRelease, "", 0 );
 })();
+
+//=============================================================================
+// Mouse Click and Zoom
+//=============================================================================
+const ZOOM_RATE = 100;
+const MAX_ZOOM = 2000;
+const MIN_ZOOM = 1000;
+
+// Other things
+var TEXT_CONVERSION = 1000;
+var ZOOM_INTERVAL = 0.03;
+var ZOOM_JUMP = 10;
+var FAST_ZOOM = 10;
+var DEFAULT_ZOOM = 1500;
+var CAMERA_DEFAULT_PITCH = 66
+
+/**
+ * Call this to set the zoom. Eventually, if they ever add
+ * camera moving functionality from panorama, I'll make it move
+ * the camera up and down so the zoom zooms in and out of the
+ * what you're currently viewing instead of just vertically.
+ */
+function SetZoom( newZoom ) {
+	GameUI.SetCameraDistance(newZoom);
+	SetPitch(CAMERA_DEFAULT_PITCH);
+	currZoom = newZoom;
+}
+
+function SetPitch( pitch ) {
+	GameUI.SetCameraPitchMin(pitch);
+	GameUI.SetCameraPitchMax(pitch);
+}
+
+var currZoom = DEFAULT_ZOOM;
+var bufferedZoom = DEFAULT_ZOOM;
+var locked = false;
+SetZoom(DEFAULT_ZOOM);
+
+function AdjustZoom() {
+	var newZoom = currZoom;
+	if (bufferedZoom > newZoom) {
+		var diff = bufferedZoom - newZoom;
+		if (diff < ZOOM_JUMP)
+			newZoom = bufferedZoom;
+		else if (diff / 10 < ZOOM_JUMP)
+			newZoom += ZOOM_JUMP;
+		else
+			newZoom += diff / 10;
+	} else if (bufferedZoom < currZoom) {
+		var diff = currZoom - bufferedZoom;
+		if (diff < ZOOM_JUMP)
+			newZoom = bufferedZoom;
+		else if (diff / 10 < ZOOM_JUMP)
+			newZoom -= ZOOM_JUMP;
+		else
+			newZoom -= diff / 10;
+	}
+	
+	SetZoom( newZoom );
+	
+	if (bufferedZoom != currZoom)
+		$.Schedule(ZOOM_INTERVAL, function(){AdjustZoom();});
+}
+
+GameUI.SetMouseCallback( function( eventName, arg ) {
+	const CONSUME_EVENT = true;
+	const CONTINUE_PROCESSING_EVENT = false;
+	
+	if (eventName === "pressed" && (arg == 5 || arg == 6) ) {
+		return CONSUME_EVENT;
+	}
+	
+	if ( eventName === "wheeled" ) {
+		// Disable scrolling when locked
+		if ( locked )
+			return CONSUME_EVENT;
+		
+		if ( arg < 0 && bufferedZoom < MAX_ZOOM ) { // Zoom out
+			bufferedZoom += ZOOM_RATE;
+		}
+		else if ( arg > 0 && bufferedZoom > MIN_ZOOM ) { // Zoom in
+			bufferedZoom -= ZOOM_RATE;
+		}
+		AdjustZoom();
+		return CONSUME_EVENT;
+	}
+	return CONTINUE_PROCESSING_EVENT;
+} );
