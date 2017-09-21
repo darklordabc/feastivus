3e80c82849eec5e9ff09135de780820bbbe144cb
CarpeSwag <james@garbagnati.com>
2017-09-21 00:52:43 -0700

    Moved code into gamemode (still probably the wrong file) and adjusted the initial wasd movement adjustment code.

--- game/frostivus/scripts/vscripts/gamemode.lua
+++ game/frostivus/scripts/vscripts/gamemode.lua
@@ -6,13 +6,13 @@ BAREBONES_VERSION = "1.00"
 BAREBONES_DEBUG_SPEW = false 
 
 if GameMode == nil then
-    DebugPrint( '[BAREBONES] creating barebones game mode' )
-    _G.GameMode = class({})
+		DebugPrint( '[BAREBONES] creating barebones game mode' )
+		_G.GameMode = class({})
 end
 
 -- This library allow for easily delayed/timed actions
 require('libraries/timers')
--- This library can be used for advancted physics/motion/collision of units.  See PhysicsReadme.txt for more information.
+-- This library can be used for advancted physics/motion/collision of units.	See PhysicsReadme.txt for more information.
 require('libraries/physics')
 -- This library can be used for advanced 3D projectile systems.
 require('libraries/projectiles')
@@ -33,7 +33,7 @@ require('libraries/pathgraph')
 -- This library (by Noya) provides player selection inspection and management from server lua
 require('libraries/selection')
 
--- These internal libraries set up barebones's events and processes.  Feel free to inspect them/change them if you need to.
+-- These internal libraries set up barebones's events and processes.	Feel free to inspect them/change them if you need to.
 require('internal/gamemode')
 require('internal/events')
 
@@ -45,116 +45,198 @@ require('events')
 
 -- This is a detailed example of many of the containers.lua possibilities, but only activates if you use the provided "playground" map
 if GetMapName() == "playground" then
-  require("examples/playground")
+	require("examples/playground")
 end
 
 --require("examples/worldpanelsExample")
 
 --[[
-  This function should be used to set up Async precache calls at the beginning of the gameplay.
+	This function should be used to set up Async precache calls at the beginning of the gameplay.
 
-  In this function, place all of your PrecacheItemByNameAsync and PrecacheUnitByNameAsync.  These calls will be made
-  after all players have loaded in, but before they have selected their heroes. PrecacheItemByNameAsync can also
-  be used to precache dynamically-added datadriven abilities instead of items.  PrecacheUnitByNameAsync will 
-  precache the precache{} block statement of the unit and all precache{} block statements for every Ability# 
-  defined on the unit.
+	In this function, place all of your PrecacheItemByNameAsync and PrecacheUnitByNameAsync.	These calls will be made
+	after all players have loaded in, but before they have selected their heroes. PrecacheItemByNameAsync can also
+	be used to precache dynamically-added datadriven abilities instead of items.	PrecacheUnitByNameAsync will 
+	precache the precache{} block statement of the unit and all precache{} block statements for every Ability# 
+	defined on the unit.
 
-  This function should only be called once.  If you want to/need to precache more items/abilities/units at a later
-  time, you can call the functions individually (for example if you want to precache units in a new wave of
-  holdout).
+	This function should only be called once.	If you want to/need to precache more items/abilities/units at a later
+	time, you can call the functions individually (for example if you want to precache units in a new wave of
+	holdout).
 
-  This function should generally only be used if the Precache() function in addon_game_mode.lua is not working.
+	This function should generally only be used if the Precache() function in addon_game_mode.lua is not working.
 ]]
 function GameMode:PostLoadPrecache()
-  DebugPrint("[BAREBONES] Performing Post-Load precache")    
-  --PrecacheItemByNameAsync("item_example_item", function(...) end)
-  --PrecacheItemByNameAsync("example_ability", function(...) end)
+	DebugPrint("[BAREBONES] Performing Post-Load precache")		
+	--PrecacheItemByNameAsync("item_example_item", function(...) end)
+	--PrecacheItemByNameAsync("example_ability", function(...) end)
 
-  --PrecacheUnitByNameAsync("npc_dota_hero_viper", function(...) end)
-  --PrecacheUnitByNameAsync("npc_dota_hero_enigma", function(...) end)
+	--PrecacheUnitByNameAsync("npc_dota_hero_viper", function(...) end)
+	--PrecacheUnitByNameAsync("npc_dota_hero_enigma", function(...) end)
 end
 
 --[[
-  This function is called once and only once as soon as the first player (almost certain to be the server in local lobbies) loads in.
-  It can be used to initialize state that isn't initializeable in InitGameMode() but needs to be done before everyone loads in.
+	This function is called once and only once as soon as the first player (almost certain to be the server in local lobbies) loads in.
+	It can be used to initialize state that isn't initializeable in InitGameMode() but needs to be done before everyone loads in.
 ]]
 function GameMode:OnFirstPlayerLoaded()
-  DebugPrint("[BAREBONES] First Player has loaded")
+	DebugPrint("[BAREBONES] First Player has loaded")
 end
 
 --[[
-  This function is called once and only once after all players have loaded into the game, right as the hero selection time begins.
-  It can be used to initialize non-hero player state or adjust the hero selection (i.e. force random etc)
+	This function is called once and only once after all players have loaded into the game, right as the hero selection time begins.
+	It can be used to initialize non-hero player state or adjust the hero selection (i.e. force random etc)
 ]]
 function GameMode:OnAllPlayersLoaded()
-  DebugPrint("[BAREBONES] All Players have loaded into the game")
+	DebugPrint("[BAREBONES] All Players have loaded into the game")
 end
 
 --[[
-  This function is called once and only once for every player when they spawn into the game for the first time.  It is also called
-  if the player's hero is replaced with a new hero for any reason.  This function is useful for initializing heroes, such as adding
-  levels, changing the starting gold, removing/adding abilities, adding physics, etc.
+	This function is called once and only once for every player when they spawn into the game for the first time.	It is also called
+	if the player's hero is replaced with a new hero for any reason.	This function is useful for initializing heroes, such as adding
+	levels, changing the starting gold, removing/adding abilities, adding physics, etc.
 
-  The hero parameter is the hero entity that just spawned in
+	The hero parameter is the hero entity that just spawned in
 ]]
 function GameMode:OnHeroInGame(hero)
-  DebugPrint("[BAREBONES] Hero spawned in game for first time -- " .. hero:GetUnitName())
+	DebugPrint("[BAREBONES] Hero spawned in game for first time -- " .. hero:GetUnitName())
 
-  -- This line for example will set the starting gold of every hero to 500 unreliable gold
-  --hero:SetGold(500, false)
+	-- This line for example will set the starting gold of every hero to 500 unreliable gold
+	--hero:SetGold(500, false)
 
-  -- These lines will create an item and add it to the player, effectively ensuring they start with the item
-  local item = CreateItem("item_example_item", hero, hero)
-  hero:AddItem(item)
+	-- These lines will create an item and add it to the player, effectively ensuring they start with the item
+	local item = CreateItem("item_example_item", hero, hero)
+	hero:AddItem(item)
 
-  --[[ --These lines if uncommented will replace the W ability of any hero that loads into the game
-    --with the "example_ability" ability
+	--[[ --These lines if uncommented will replace the W ability of any hero that loads into the game
+		--with the "example_ability" ability
 
-  local abil = hero:GetAbilityByIndex(1)
-  hero:RemoveAbility(abil:GetAbilityName())
-  hero:AddAbility("example_ability")]]
+	local abil = hero:GetAbilityByIndex(1)
+	hero:RemoveAbility(abil:GetAbilityName())
+	hero:AddAbility("example_ability")]]
 end
 
 --[[
-  This function is called once and only once when the game completely begins (about 0:00 on the clock).  At this point,
-  gold will begin to go up in ticks if configured, creeps will spawn, towers will become damageable etc.  This function
-  is useful for starting any game logic timers/thinkers, beginning the first round, etc.
+	This function is called once and only once when the game completely begins (about 0:00 on the clock).	At this point,
+	gold will begin to go up in ticks if configured, creeps will spawn, towers will become damageable etc.	This function
+	is useful for starting any game logic timers/thinkers, beginning the first round, etc.
 ]]
 function GameMode:OnGameInProgress()
-  DebugPrint("[BAREBONES] The game has officially begun")
+	DebugPrint("[BAREBONES] The game has officially begun")
 
-  Timers:CreateTimer(30, -- Start this timer 30 game-time seconds later
-    function()
-      DebugPrint("This function is called 30 seconds after the game begins, and every 30 seconds thereafter")
-      return 30.0 -- Rerun this timer every 30 game-time seconds 
-    end)
+	Timers:CreateTimer(30, -- Start this timer 30 game-time seconds later
+		function()
+			DebugPrint("This function is called 30 seconds after the game begins, and every 30 seconds thereafter")
+			return 30.0 -- Rerun this timer every 30 game-time seconds 
+		end)
 end
 
+UP = '0'
+LEFT = '1'
+DOWN = '2'
+RIGHT = '3'
 
+reverseDir = {
+	[UP] = '2',
+	[LEFT] = '3',
+	[DOWN] = '0',
+	[RIGHT] = '1'
+}
+
+dir = {
+	[UP] = 0,
+	[LEFT] = 0,
+	[DOWN] = 0,
+	[RIGHT] = 0
+}
+
+delayDir = {
+	[UP] = 0,
+	[LEFT] = 0,
+	[DOWN] = 0,
+	[RIGHT] = 0
+}
+
+SPEED = 360 / 30
 
 -- This function initializes the game mode and is called before anyone loads into the game
 -- It can be used to pre-initialize any values/tables that will be needed later
 function GameMode:InitGameMode()
-  GameMode = self
-  DebugPrint('[BAREBONES] Starting to load Barebones gamemode...')
+	GameMode = self
+	DebugPrint('[BAREBONES] Starting to load Barebones gamemode...')
 
-  -- Commands can be registered for debugging purposes or as functions that can be called by the custom Scaleform UI
-  Convars:RegisterCommand( "command_example", Dynamic_Wrap(GameMode, 'ExampleConsoleCommand'), "A console command example", FCVAR_CHEAT )
+	-- Commands can be registered for debugging purposes or as functions that can be called by the custom Scaleform UI
+	Convars:RegisterCommand( "command_example", Dynamic_Wrap(GameMode, 'ExampleConsoleCommand'), "A console command example", FCVAR_CHEAT )
 
-  DebugPrint('[BAREBONES] Done loading Barebones gamemode!\n\n')
+	DebugPrint('[BAREBONES] Done loading Barebones gamemode!\n\n')
+	
+	
+	Timers:CreateTimer(1/30, function()
+		local hero = PlayerResource:GetSelectedHeroEntity(0)
+		if not hero then return 1/30 end
+		
+		hero:SetModelScale(2)
+		
+		local nextPos = Vector(
+			dir[RIGHT] + delayDir[RIGHT] - dir[LEFT] - delayDir[LEFT],
+			dir[UP] + delayDir[UP] - dir[DOWN] - delayDir[DOWN],
+			0
+		)
+		
+		if (nextPos:Length() == 0) then
+			--hero:Stop()
+			return 1/30
+		end
+		
+		nextPos = (nextPos / nextPos:Length()) * SPEED
+		
+		hero:FaceTowards(hero:GetAbsOrigin() + nextPos)
+		
+		if delayDir[RIGHT] - delayDir[LEFT] ~= 0
+			or delayDir[UP] - delayDir[DOWN] ~= 0 then
+			hero:SetAbsOrigin(hero:GetAbsOrigin() + nextPos)
+		end
+		
+		delayDir = {
+			[UP] = 0,
+			[LEFT] = 0,
+			[DOWN] = 0,
+			[RIGHT] = 0
+		}
+		
+		hero:MoveToPosition(hero:GetAbsOrigin() + nextPos)
+		
+		return 1/30
+	end)
 end
+
+-- Testing shit
+function KeyDown( eventSourceIndex, args )
+	local direction = args.dir
+	dir[direction] = 1
+	if dir[reverseDir[direction]] == 0 then
+		delayDir[direction] = 2
+	end
+end
+
+function KeyUp( eventSourceIndex, args )
+	local direction = args.dir
+	dir[direction] = 0
+end
+ 
+CustomGameEventManager:RegisterListener( "keyDown", KeyDown )
+CustomGameEventManager:RegisterListener( "keyUp", KeyUp )
 
 -- This is an example console command
 function GameMode:ExampleConsoleCommand()
-  print( '******* Example Console Command ***************' )
-  local cmdPlayer = Convars:GetCommandClient()
-  if cmdPlayer then
-    local playerID = cmdPlayer:GetPlayerID()
-    if playerID ~= nil and playerID ~= -1 then
-      -- Do something here for the player who called this command
-      PlayerResource:ReplaceHeroWith(playerID, "npc_dota_hero_viper", 1000, 1000)
-    end
-  end
+	print( '******* Example Console Command ***************' )
+	local cmdPlayer = Convars:GetCommandClient()
+	if cmdPlayer then
+		local playerID = cmdPlayer:GetPlayerID()
+		if playerID ~= nil and playerID ~= -1 then
+			-- Do something here for the player who called this command
+			PlayerResource:ReplaceHeroWith(playerID, "npc_dota_hero_viper", 1000, 1000)
+		end
+	end
 
-  print( '*********************************************' )
+	print( '*********************************************' )
 end
