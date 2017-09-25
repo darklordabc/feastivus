9fc46f117c39af388aae70a9334570822a28d66e
Wouter <wouter.zegers@gmail.com>
2017-09-25 19:25:56 +0200

    Fixed a syntax error

--- game/frostivus/scripts/vscripts/frostivus/objects/treadmill.lua
+++ game/frostivus/scripts/vscripts/frostivus/objects/treadmill.lua
@@ -1,8 +1,7 @@
 function Objects:ManageTreadmill(object)
-  local origin = treadmill:GetAbsOrigin()
   local flSpeed = 3
   for k,v in pairs(Objects:FindUnitsOnTop(object)) do
-    v:SetAbsOrigin(v:GetAbsOrigin+object:GetForwardVector()*flSpeed)
+    v:SetAbsOrigin(v:GetAbsOrigin()+object:GetForwardVector()*flSpeed)
     -- Particle effect?
   end
 end
