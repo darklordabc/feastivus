9e900921af144854455b5c0d18056e1e8bf0ad5e
TideSofDarK <tidesmain@gmail.com>
2017-10-20 03:16:42 +0500

    Recipes

--- game/frostivus/scripts/vscripts/frostivus/objects/plate_bench.lua
+++ game/frostivus/scripts/vscripts/frostivus/objects/plate_bench.lua
@@ -1,3 +1,39 @@
+function deepcompare(t1,t2,ignore_mt)
+	local ty1 = type(t1)
+	local ty2 = type(t2)
+	if ty1 ~= ty2 then return false end
+	-- non-table types can be directly compared
+	if ty1 ~= 'table' and ty2 ~= 'table' then return t1 == t2 end
+	-- as well as tables which have the metamethod __eq
+	local mt = getmetatable(t1)
+	if not ignore_mt and mt and mt.__eq then return t1 == t2 end
+	for k1,v1 in pairs(t1) do
+		local v2 = t2[k1]
+		if v2 == nil or not deepcompare(v1,v2) then return false end
+	end
+	for k2,v2 in pairs(t2) do
+		local v1 = t1[k2]
+		if v1 == nil or not deepcompare(v1,v2) then return false end
+	end
+	return true
+end
+
+function CheckRecipe(items, recipe)
+	local function IDArray( a )
+		local new = {}
+		for k,v in pairs(a) do
+			table.insert(new, Frostivus.ItemsKVs[v].ID)
+		end
+		table.sort(new)
+		return new
+	end
+
+	local v1 = IDArray( items )
+	local v2 = IDArray( recipe )
+
+	return deepcompare(v1,v2,true)
+end
+
 function InitBench( keys )
 	local caster = keys.caster
 	local ability = keys.ability
@@ -29,13 +65,24 @@ function InitBench( keys )
 				
 			end)
 			plate:SetOnBenchIsFull( function ( bench, items, user )
-				local dish = CreateItemOnPositionSync(plate:GetAbsOrigin(),CreateItem(bench.current_item,nil,nil))
+				local result
 
-				bench._holder:RemoveModifierByName("modifier_carrying_item")
-				bench._holder:PickItemFromBench(user, plate):RemoveSelf()
+				for k,v in pairs(Frostivus.RecipesKVs["1"]) do
+					if CheckRecipe(items, v.Assembly) then
+						result = k
+						break
+					end
+				end
 
-				bench._holder:AddItemToBench(bench.current_item, user)
-				bench._holder:BindItem(dish)
+				if result then
+					local dish = CreateItemOnPositionSync(plate:GetAbsOrigin(),CreateItem(result,nil,nil))
+
+					bench._holder:RemoveModifierByName("modifier_carrying_item")
+					bench._holder:PickItemFromBench(user, plate):RemoveSelf()
+
+					bench._holder:AddItemToBench(result, user)
+					bench._holder:BindItem(dish)
+				end
 			end )
 			
 			if not self._bench_infinite_items then
@@ -53,31 +100,15 @@ end
 
 function CheckItem( bench, item )
 	local item_name = item:GetContainedItem():GetName()
-	local allow = false
 
-	if not bench.current_item then
-		for k,v in pairs(g_GetCurrentOrders()) do
-			if allow then
-				break
-			end
-			for k1,v1 in pairs(Frostivus.RecipesKVs["1"][v.pszItemName].Assembly) do
+	for k,v in pairs(Frostivus.RecipesKVs["1"]) do
+		for k1,v1 in pairs(v.Assembly) do
 
-				if v1 == item_name then
-					allow = true
-					bench.current_item = v.pszItemName
-					break
-				end
-			end
-		end
-	else
-		-- TO DO: recipe checking
-		for k1,v1 in pairs(Frostivus.RecipesKVs["1"][bench.current_item].Assembly) do
 			if v1 == item_name then
-				allow = true
-				break
+				return true
 			end
 		end
 	end
 
-	return allow
+	return false
 end
