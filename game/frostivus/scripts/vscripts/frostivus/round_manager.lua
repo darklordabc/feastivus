9819b5092a79034323570a7c1d7aae030ff7b0db
XavierCHN <Xavier_CHN@live.com>
2017-10-20 09:00:09 +0800

    Fixes #19

--- game/frostivus/scripts/vscripts/frostivus/round_manager.lua
+++ game/frostivus/scripts/vscripts/frostivus/round_manager.lua
@@ -168,8 +168,10 @@ function Round:OnTimer()
 	self.nCountDownTimer = self.nCountDownTimer - 1
 	self.nExpiredTime = self.nExpiredTime + 1
 
-	-- time's up or there are no pending orders, finish this round
-	if self.nCountDownTimer <= 0 or table.count(self.vPendingOrders) <= 0 then
+	-- time's up or there are no pending orders and no orders running
+	-- this round is ended
+	if self.nCountDownTimer <= 0 or (table.count(self.vPendingOrders) <= 0 and table.count(self.vCurrentOrders) <= 0) then
+		self.nCountDownTime = 0
 		self:EndRound()
 	end
 
