4b10260fffde0a7954fdc6e964c98ace157304f4
XavierCHN <Xavier_CHN@live.com>
2017-10-24 14:41:32 +0800

    disable all music of valve music packs

--- game/frostivus/sounds/create_empty_files_vo.py
+++ game/frostivus/sounds/create_empty_files_vo.py
@@ -1,10 +1,14 @@
 import os
-rootdir = 'music/valve_dota_001/music'
+rootdir = 'music'
 list = os.listdir(rootdir) 
 for i in range(0,len(list)):
 	path = os.path.join(rootdir,list[i])
-	if os.path.isfile(path):
-		print("finished", path)
-		open(path, "wb").write(open("null.vsnd_c", "rb").read())
+	if not os.path.isfile(path):
+		print(path)
+		file_list = os.listdir(path + "/stingers")
+		for file in file_list:
+			open(path + "/stingers/" + file, "wb").write(open("null.vsnd_c", "rb").read())
+		# print("finished", path)
+		# open(path, "wb").write(open("null.vsnd_c", "rb").read())
 		
 
