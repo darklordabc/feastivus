e74f08d1903de21e84576399466353b94df4d4bf
Xavier Zheng <Xavier@dota2x.cn>
2017-11-06 19:28:43 +0800

    finished tutorial hints

--- content/frostivus/panorama/scripts/custom_game/frostivus/message_center.js
+++ content/frostivus/panorama/scripts/custom_game/frostivus/message_center.js
@@ -1,23 +1,58 @@
-function AddMessage() {
+var m_AllMessagePanels = { };
 
-	var duration = 5;
+function OnShowMessage(args) {
+	var id = args.id;
+	var item = args.item;
+	var icon = args.icon;
+	var text = args.text;
+	var duration = args.duration
+
+	$.Msg("OnDemandShowMessage", args)
+
 	var panel = $.CreatePanel("Panel", $("#message_center_panel"), "");
 	panel.BLoadLayoutSnippet("Message");
-	panel.AddClass("Negative");
-	panel.DeleteAsync(duration)
+
+	if (item !== undefined) {
+		panel.FindChildTraverse("images_panel").RemoveClass("NoImage");
+		panel.FindChildTraverse("item_image").RemoveClass("NoImage");
+		panel.FindChildTraverse("item_image").itemname = item;
+	}
+	
+	if (icon !== undefined) {
+		panel.FindChildTraverse("images_panel").RemoveClass("NoImage");
+		panel.FindChildTraverse("normal_image").RemoveClass("NoImage");
+		panel.FindChildTraverse("normal_image").SetImage("file://{resources}/images/custom_game/msgs/" + icon);
+	}
+
+	panel.FindChildTraverse("message_text").text = $.Localize(text);
+
+	if (duration != undefined) {
+		$.Schedule(duration - 0.5, function(){panel.AddClass("Fadeout"); });
+		panel.DeleteAsync(duration)
+	}else{
+		// store the id for delete
+		m_AllMessagePanels[id] = panel;
+	}
+
+	// fail safe delele message in 30 seconds
+	$.Schedule(30, function(){
+		if (m_AllMessagePanels[id] != undefined ) {
+			m_AllMessagePanels[id].AddClass("Fadeout");
+			m_AllMessagePanels[id].DeleteAsync(0.5);
+		} 
+	});
+}
+
+function OnRemoveMessage(args) {
+	var id = args.id
+	if (m_AllMessagePanels[id] != null){
+		m_AllMessagePanels[id].AddClass("Fadeout");
+		m_AllMessagePanels[id].DeleteAsync(0.5);
+		delete m_AllMessagePanels[id];
+	}
 }
 
 (function() {
-	$.Schedule(2, function() {
-		AddMessage();
-	})
-	$.Schedule(3, function() {
-		AddMessage();
-	})
-	$.Schedule(5, function() {
-		AddMessage();
-	})
-	$.Schedule(8, function() {
-		AddMessage();
-	})
+	GameEvents.Subscribe('frostivus_show_message', OnShowMessage);
+	GameEvents.Subscribe('frostivus_remove_message', OnRemoveMessage);
 })();
