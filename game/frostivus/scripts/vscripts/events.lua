dfe83d0d9dd323c0e0f93a817ce87bf8a897cb81
Xavier Zheng <Xavier@dota2x.cn>
2017-11-18 20:28:17 +0800

    for test

--- game/frostivus/scripts/vscripts/events.lua
+++ game/frostivus/scripts/vscripts/events.lua
@@ -334,7 +334,7 @@ end
 function GameMode:OnPlayerChat(keys)
   local teamonly = keys.teamonly
   local userID = keys.userid
-  local playerID = self.vUserIds[userID]:GetPlayerID()
+  -- local playerID = self.vUserIds[userID]:GetPlayerID()
 
   local text = keys.text
 end
